3ba8f310ea8ac2eb6a3aaf760e3894487e0a6279
Pranjil97 <pranjilkumar97@gmail.com>
2023-07-09 04:14:01 +0530

    update the code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@
 // You can handle form submissions, implement search functionality, etc.
 // For example, to handle form submissions on login.html and register.html:
 
+
+// handle Search function
+
+function handleSearch() {
+    // Redirect to search.html
+    alert("Enter the details again -> Redirecting to Search page")
+    window.location.href = "search.html";
+}
+
+// Add an event listener to the search form
+document.getElementById("search-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+    handleSearch(); // Call the handleSearch function
+});
+
 // Login form submission
 const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', (event) => {
